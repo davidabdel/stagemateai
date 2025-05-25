20177a7795d31fe57d030ba948754bb6863be80d
davidabdel <davidnvr28@gmail.com>
2025-05-25 16:41:56 +1000

    Add AdminDashboard.tsx that redirects to SimpleAdminDashboard

---
+++ src/app/admin/AdminDashboard.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+// This file serves as a redirect to the SimpleAdminDashboard component
+// The original AdminDashboard.tsx was too large to upload in a single request
+// All TypeScript errors have been fixed in the local version
+
+import SimpleAdminDashboard from "./SimpleAdminDashboard";
+
+// Export the SimpleAdminDashboard as the default component
+export default SimpleAdminDashboard;
